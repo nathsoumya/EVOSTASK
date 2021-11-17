3ab3d9b7fe84917a5baf49f9c5ddd79e3d9d9501
nathsoumya <nathsoumya1998@gmail.com>
2021-11-17 13:16:26 +0530

    Add files via upload

---
+++ settings.dart
@@ -0,0 +1,4 @@
+/// Define App ID and Token
+const APP_ID = "bc160cc708ab4b3391bc4c5766ecedcf";
+const Token = "006ed0ff08a69e84f68b5e00759d1eacce8IAAPIgrAtpjwZafsu6vn5iXxD8a6P1i/NwP116PyTUlkdB80m/YAAAAAEAB5UzQCmMaVYQEAAQCXxpVh";
+// client side token used
